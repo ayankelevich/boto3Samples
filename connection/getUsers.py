4de5446b7ae315eb55039b2bdb4e1db00ca49634
Alex Yankelevich <alex@12chairs.com>
2020-01-05 23:13:58 -0500

    Initial commit

---
+++ connection/getUsers.py
@@ -0,0 +1,39 @@
+#!/Users/alex/venv_boto3/bin/python
+
+# the line above plus chmod + x allows executing it by itself without using python3 ec2connection.py
+
+import boto3
+# for beautified JSON print
+from pprint import pprint
+
+iam_console_client = boto3.client('iam')
+
+users = iam_console_client.list_users()
+
+print(users['Users'])
+
+# Print list example (heavy-handed)
+user_names = list(map(lambda x: x['UserName'], users['Users']))
+print(user_names)
+# Get names
+for x in users['Users']:
+    print(x['UserName'])
+
+ec2_console_client = boto3.client('ec2')
+ec2s = ec2_console_client.describe_instances()
+# for beautified JSON print
+pprint(ec2s)
+
+# does not work <===================
+s3_console_resource = boto3.resource('s3')
+filters = [{'name': 'name', 'value': 'twelvechairs.com'}]
+bucket_list_filtered = s3_console_resource.buckets.filter(Filter=filters)
+for bucket in bucket_list_filtered:
+    print(bucket.name, bucket.creation_date)
+
+sts_console_client = boto3.client('sts')
+response = sts_console_client.get_caller_identity()
+print(response['UserId'], response['Account'], response['Arn'])
+
+# Example of using client
+# ec2_con_cli.start_instances(InstanceIds=[instance_id])
---
+++ connection/waiters.py
@@ -0,0 +1,33 @@
+import boto3
+from pprint import pprint
+
+# Plug in your instance id here
+instance_id = 'i-081571aadebf49f2c'
+
+aws_con = boto3.session.Session()
+ec2_con_re = aws_con.resource(service_name="ec2", region_name="us-east-1")
+ec2_con_cli = aws_con.client(service_name="ec2", region_name="us-east-1")
+pprint(ec2_con_cli.waiter_names)
+
+# client
+print("Starting ec2 instance {}".format(instance_id))
+ec2_con_cli.start_instances(InstanceIds=[instance_id])
+waiter_running = ec2_con_cli.get_waiter('instance_running')
+waiter_running.wait(InstanceIds=[instance_id])  # 40 checks after every 15 sec
+print("Now your ec2 instance is up and running")
+
+print("Stopping ec2 instance {}".format(instance_id))
+ec2_con_cli.stop_instances(InstanceIds=[instance_id])
+waiter_stopped = ec2_con_cli.get_waiter('instance_stopped')
+waiter_stopped.wait(InstanceIds=[instance_id])
+
+# resource
+my_inst_ob = ec2_con_re.Instance(instance_id)
+print("Starting ec2 instance {}".format(instance_id))
+my_inst_ob.start()
+waiter_running.wait(InstanceIds=[instance_id])
+print("Now your ec2 instance is up and running again")
+
+print("Stopping ec2 instance {}".format(instance_id))
+ec2_con_cli.stop_instances(InstanceIds=[instance_id])
+waiter_stopped.wait(InstanceIds=[instance_id])
