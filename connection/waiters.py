ca1ce3105317884294c60bfca0799e6d72525e80
Alex Yankelevich <alex@12chairs.com>
2020-01-11 15:11:14 -0500

    Initial commit

--- connection/waiters.py
+++ connection/waiters.py
@@ -13,19 +13,19 @@ pprint(ec2_con_cli.waiter_names)
 print("Starting ec2 instance {}".format(instance_id))
 ec2_con_cli.start_instances(InstanceIds=[instance_id])
 waiter_running = ec2_con_cli.get_waiter('instance_running')
-waiter_running.wait(InstanceIds=[instance_id])  # 40 checks after every 15 sec
+waiter_running.wait(InstanceIds=[instance_id])  # 40 checks after every 15 sec.
 print("Now your ec2 instance is up and running")
 
 print("Stopping ec2 instance {}".format(instance_id))
 ec2_con_cli.stop_instances(InstanceIds=[instance_id])
-waiter_stopped = ec2_con_cli.get_waiter('instance_stopped')
+waiter_stopped = ec2_con_cli.get_waiter('instance_stopped')  # 40 checks after every 5 sec.
 waiter_stopped.wait(InstanceIds=[instance_id])
 
 # resource
 my_inst_ob = ec2_con_re.Instance(instance_id)
 print("Starting ec2 instance {}".format(instance_id))
 my_inst_ob.start()
-waiter_running.wait(InstanceIds=[instance_id])
+my_inst_ob.wait_until_running()  # client waiter can also be attached
 print("Now your ec2 instance is up and running again")
 
 print("Stopping ec2 instance {}".format(instance_id))
